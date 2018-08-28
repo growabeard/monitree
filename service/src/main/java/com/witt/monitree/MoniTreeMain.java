b005fd9a4e3cbc69f138b917bad11d915fd9c1a2
Chris Witt <christopher.witt@gm.com>
2018-08-28 11:00:13 -0400

    Initializing code for project.

---
+++ service/src/main/java/com/witt/monitree/MoniTreeMain.java
@@ -0,0 +1,11 @@
+package com.witt.monitree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoniTreeMain {
+    public static void main(String[] args) {
+        SpringApplication.run(MoniTreeMain.class, args);
+    }
+}
