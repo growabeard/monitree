7d128d14c610a3b2ae39aba8eaba318e551e0067
growabeard <chris.w.witt@gmail.com>
2020-04-13 10:29:51 -0400

    Fixing imports.

--- src/main/java/com/witt/monitree/api/ReadingsApiController.java
+++ src/main/java/com/witt/monitree/api/ReadingsApiController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.witt.monitree.delegate.ReadingService;
 import com.witt.monitree.model.Reading;
